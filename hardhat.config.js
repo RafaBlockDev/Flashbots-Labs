c541715d336f1e9668dd3e79abe662560b83c85d
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-06-14 23:59:36 -0500

    Config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,36 +5,29 @@ require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    mumbai: {
+      url: process.env.ALCHEMY_MUMBAI_URL,
       accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        [process.env.PRIVATE_KEY_MUMBAI]
     },
   },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 500
+    }
+  },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
+    srcPath: "./contracts",
     currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+    gasPrice: 21,
+    enabled: true,
+    token: 'ETH',
+    gasPriceAPI: "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice",
+    showTimeSpent: true,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY
+  }
 };
